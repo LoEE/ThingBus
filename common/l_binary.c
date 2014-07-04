49783c28a727fedb60cd31034261e25761a5d359
Jakub Piotr Cłapa <jpc@loee.pl>
2014-07-04 17:41:26 +0200

    Add float and double (un)packing to l_binary.

--- common/l_binary.c
+++ common/l_binary.c
@@ -62,9 +62,56 @@ static lua_Number extract_number_64 (const uint8_t *s, size_t n, int le, int iss
   return (int64_t)r;
 }
 
+static lua_Number extract_float (const uint8_t *s, int flipendian)
+{
+  const size_t n = sizeof(float);
+  union { float v; char c[n]; } u;
+  if (flipendian)
+    for (size_t i = 0; i < n; i++) u.c[n-1-i] = *s++;
+  else
+    for (size_t i = 0; i < n; i++) u.c[i] = *s++;
+  return u.v;
+}
+
+static lua_Number extract_double (const uint8_t *s, int flipendian)
+{
+  const size_t n = sizeof(double);
+  union { double v; char c[n]; } u;
+  if (flipendian)
+    for (size_t i = 0; i < n; i++) u.c[n-1-i] = *s++;
+  else
+    for (size_t i = 0; i < n; i++) u.c[i] = *s++;
+  return u.v;
+}
+
+static size_t inject_float (uint8_t *s, int flipendian, lua_Number v)
+{
+  const size_t n = sizeof(float);
+  union { float v; char c[n]; } u;
+  u.v = v;
+  if (flipendian)
+    for (size_t i = 0; i < n; i++) *s++ = u.c[n-1-i];
+  else
+    for (size_t i = 0; i < n; i++) *s++ = u.c[i];
+  return n;
+}
+
+static size_t inject_double (uint8_t *s, int flipendian, lua_Number v)
+{
+  const size_t n = sizeof(double);
+  union { double v; char c[n]; } u;
+  u.v = v;
+  if (flipendian)
+    for (size_t i = 0; i < n; i++) *s++ = u.c[n-1-i];
+  else
+    for (size_t i = 0; i < n; i++) *s++ = u.c[i];
+  return n;
+}
+
 size_t lua_binary_unpack_ll (lua_State *L, const uint8_t *s, size_t n, const char *f, int *results)
 {
-  int le = is_little_endian();
+  int nativele = is_little_endian();
+  int le = nativele;
   size_t startn = n;
   int starti = lua_gettop(L);
   while (1) {
@@ -84,6 +131,15 @@ size_t lua_binary_unpack_ll (lua_State *L, const uint8_t *s, size_t n, const cha
           lua_pushnumber (L, extract_number (s, size, le, *c == 's'));
         else
           lua_pushnumber (L, extract_number_64 (s, size, le, *c == 's'));
+        s += size; n -= size;
+        break;
+      case 'f':
+        if(size == 4)
+          lua_pushnumber(L, extract_float (s, le != nativele));
+        else if (size == 8)
+          lua_pushnumber(L, extract_double (s, le != nativele));
+        else
+          return luaL_error(L, "the size of a float must be 4 or 8 bytes");
         s += size; n -= size;
         break;
       case 'c':
@@ -138,6 +194,32 @@ static int lua_binary_unpack (lua_State *L)
   size_t n = lua_binary_unpack_ll (L, (const uint8_t *)s, size, fmt, &results);
   lua_pushnumber (L, n); lua_insert (L, results_start + 1);
   return results + 1;
+}
+
+static int lua_binary_packfloat (lua_State *L)
+{
+  int nativele = is_little_endian();
+  int le = nativele;
+
+  lua_Number v = lua_tonumber (L, 1);
+  size_t n = lua_tonumber (L, 2);
+  const char *endianess = lua_tostring (L, 3);
+  if (endianess) {
+    if (*endianess == '<') le = 1;
+    else if (*endianess == '>') le = 0;
+    else return luaL_argerror (L, 3, "unknown endianess");
+  }
+
+  uint8_t buf[8];
+  if (n == 4)
+    inject_float(buf, le != nativele, v);
+  else if (n == 8)
+    inject_double(buf, le != nativele, v);
+  else
+    return luaL_argerror(L, 2, "the size of a float must be 4 or 8 bytes");
+
+  lua_pushlstring(L, (char *)buf, n);
+  return 1;
 }
 
 static int lua_binary_unpackbits (lua_State *L)
@@ -234,6 +316,7 @@ static int lua_binary_strxor (lua_State *L)
 static const struct luaL_reg functions[] = {
   {"unpack",     lua_binary_unpack     },
   {"unpackbits", lua_binary_unpackbits },
+  {"packfloat",  lua_binary_packfloat  },
   {"b64_encode", lua_binary_b64_encode },
   {"b64_decode", lua_binary_b64_decode },
   {"strxor",     lua_binary_strxor     },
