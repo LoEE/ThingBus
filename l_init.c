LoEE/ThingBus

commit 0196aed63205be4c3043e4325833f73198c3807e

bit32, coxpcall: do not set global variables

l_init.c
@@ -468,6 +468,7 @@ static char code[] = "\n\n\n\n\n"
   "  -------------------------------------------------------------------------------\n"
   "  -- Implements xpcall with coroutines\n"
   "  -------------------------------------------------------------------------------\n"
+  "  local M = {}\n"
   "  local performResume, handleReturnValue\n"
   "  local oldpcall, oldxpcall = pcall, xpcall\n"
   "  \n"
@@ -486,7 +487,7 @@ static char code[] = "\n\n\n\n\n"
   "      return handleReturnValue(err, co, coroutine.resume(co, ...))\n"
   "  end    \n"
   "  \n"
-  "  function coxpcall(f, err, ...)\n"
+  "  function M.coxpcall(f, err, ...)\n"
   "      local res, co = oldpcall(coroutine.create, f)\n"
   "      if not res then\n"
   "          local params = {...}\n"
@@ -504,9 +505,11 @@ static char code[] = "\n\n\n\n\n"
   "    return ...\n"
   "  end\n"
   "  \n"
-  "  function copcall(f, ...)\n"
+  "  function M.copcall(f, ...)\n"
   "      return coxpcall(f, id, ...)\n"
   "  end\n"
+  "  \n"
+  "  return M\n"
   "end\n"
   "package.preload[\"md5\"] = function (...)\n"
   "  ----------------------------------------------------------------------------\n"

lualib-vendor/coxpcall.lua
@@ -16,6 +16,7 @@
 -------------------------------------------------------------------------------
 -- Implements xpcall with coroutines
 -------------------------------------------------------------------------------
+local M = {}
 local performResume, handleReturnValue
 local oldpcall, oldxpcall = pcall, xpcall
 
@@ -34,7 +35,7 @@ function performResume(err, co, ...)
     return handleReturnValue(err, co, coroutine.resume(co, ...))
 end    
 
-function coxpcall(f, err, ...)
+function M.coxpcall(f, err, ...)
     local res, co = oldpcall(coroutine.create, f)
     if not res then
         local params = {...}
@@ -52,6 +53,8 @@ local function id(trace, ...)
   return ...
 end
 
-function copcall(f, ...)
+function M.copcall(f, ...)
     return coxpcall(f, id, ...)
 end
+
+return M

common/lbitlib.c
@@ -366,7 +366,8 @@ static const luaL_Reg bitlib[] = {
 
 
 LUAMOD_API int luaopen_bit32 (lua_State *L) {
-  luaL_newlib(L, bitlib);
+  lua_newtable (L);
+  luaL_register (L, NULL, bitlib);
   return 1;
 }
 

lualib/thread.lua
@@ -25,9 +25,9 @@ local Thread = {
   identity = function (...) return ... end,
 }
 if not jit then
-  require 'coxpcall'
-  Thread.pcall = copcall
-  Thread.xpcall = coxpcall
+  local coxpcall = require 'coxpcall'
+  Thread.pcall = coxpcall.copcall
+  Thread.xpcall = coxpcall.coxpcall
 else
   Thread.pcall = pcall
   Thread.xpcall = xpcall

commit 8af5cc73bf33aa34328dd3aff4070024e67afbbe

Fixed a regression introduced in 0196aed6

l_init.c
@@ -508,7 +508,7 @@ static char code[] = "\n\n\n\n\n"
   "  end\n"
   "  \n"
   "  function M.copcall(f, ...)\n"
-  "      return coxpcall(f, id, ...)\n"
+  "      return M.coxpcall(f, id, ...)\n"
   "  end\n"
   "  \n"
   "  return M\n"

lualib-vendor/coxpcall.lua
@@ -54,7 +54,7 @@ local function id(trace, ...)
 end
 
 function M.copcall(f, ...)
-    return coxpcall(f, id, ...)
+    return M.coxpcall(f, id, ...)
 end
 
 return M